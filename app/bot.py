95b073b04f698c0282c42ec9a3a52e81b140d431
nganphan123 <jill01009@gmail.com>
2022-02-15 16:50:26 -0800

    Add exit option

--- app/bot.py
+++ app/bot.py
@@ -5,30 +5,31 @@ class Bot:
 
     def greeting(self):
         print("Hi!")
-        options = "Please choose from one of the following options:\n a)Store information\n b)Product information\n c)Other concerns"
-        print(options)
+        options = "Please choose from one of the following options:\n a)Store information\n b)Product information\n c)Other concerns\n d)Exit"
         while(True):
+            print(options)
             userInput = input().lower()
-            if(userInput not in ["a","b","c","a)","b)","c)"]):
+            if(userInput not in ["a","b","c","d","a)","b)","c)","d)"]):
                 print("Incorrect input! Please enter a,b or c: ")
             else:
-                if(userInput == ["a","a)"]):
+                if(userInput in ["a","a)"]):
                     self.getStoreInfo()
-                elif(userInput == ["c","c)"]):
+                elif(userInput in ["b","b)"]):
+                    self.getProductInfo()
+                elif(userInput in ["c","c)"]):
                     self.getCustomerService()
                 else:
-                    self.getProductInfo()
-               # print("Is there anything else I could help you with? (Yes/No)")
-                #userInput = lower(str(input()))
-                #if(userInput not in ["yes","no"]):
-                  #  print()
+                    print("Goodbye!")
+                    break
+                print("Is there anything else I could help you with?")
 
 
-    def getStoreInfo():
+
+    def getStoreInfo(self):
         print("store info")
 
-    def getCustomerService():
+    def getCustomerService(self):
         print("customer service info")
     
-    def getProductInfo():
+    def getProductInfo(self):
         print("product info")
