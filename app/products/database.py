2fa2420db26e1f0427a36addde76da987ef49062
Thuan Vo <thuan.votann@gmail.com>
2022-02-20 11:04:27 -0800

    Add skeleton for Database class

---
+++ app/products/database.py
@@ -0,0 +1,18 @@
+
+import sqlite3
+import os
+
+
+class Database:
+
+    def __init__(self, url=None, uid=None, password=None, db_name="temp"):
+        pass
+
+    def connect(self):
+        pass
+
+    def close(self):
+        pass
+
+    def execute(self, query):
+        pass
