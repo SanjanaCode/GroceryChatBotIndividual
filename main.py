794bbd32c6b40d1a1df828bf094a34c3c198f1ad
nganphan123 <jill01009@gmail.com>
2022-02-20 10:40:15 -0800

    Modify the main

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# import modules used here -- sys is a very standard one
-import sys
 from app.bot import Bot
 
-# Gather our code in a main() function
 def main():
   bot = Bot()
 
-# Standard boilerplate to call the main() function to begin
-# the program.
 if __name__ == '__main__':
   main()
