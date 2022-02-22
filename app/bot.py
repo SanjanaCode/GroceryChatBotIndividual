93f22d7ddc2d42964749a0a84a959169d234522f
SanjanaCode <sanjanareddy.feb2001@gmail.com>
2022-02-21 18:24:33 -0800

    added intent detector to bot

--- app/bot.py
+++ app/bot.py
@@ -1,3 +1,4 @@
+from google.cloud import dialogflow
 class Bot:
 
     # def __init__(self):
@@ -34,36 +35,32 @@ class Bot:
     # def getProductInfo(self):
     #     print("product info")
 
-    project_id = "grocery-chat-bot"
-    session_id = "test"
-    texts = "Hello"
-    def detect_intent_texts(text, project_id = "grocery-chat-bot", session_id = "test", language_code = "en-US"):
-        """Returns the result of detect intent with texts as inputs.
+    
+    def detect_intent_texts(self,text, project_id = "grocery-chat-bot", session_id = "test", language_code = "en-US"):
+      """Returns the result of detect intent with texts as inputs.
 
-        Using the same `session_id` between requests allows continuation
-        of the conversation."""
-        from google.cloud import dialogflow
+      Using the same `session_id` between requests allows continuation
+      of the conversation."""
+      
+      session_client = dialogflow.SessionsClient()
 
-        session_client = dialogflow.SessionsClient()
+      session = session_client.session_path(project_id, session_id)
+      print("Session path: {}\n".format(session))
 
-        session = session_client.session_path(project_id, session_id)
-        print("Session path: {}\n".format(session))
+      text_input = dialogflow.TextInput(text=text, language_code=language_code)
 
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+      query_input = dialogflow.QueryInput(text=text_input)
 
-        query_input = dialogflow.QueryInput(text=text_input)
+      response = session_client.detect_intent(
+          request={"session": session, "query_input": query_input}
+      )
 
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-
-        print("=" * 20)
-        print("Query text: {}".format(response.query_result.query_text))
-        print(
-            "Detected intent: {} (confidence: {})\n".format(
-                response.query_result.intent.display_name,
-                response.query_result.intent_detection_confidence,
-            )
-        )
-        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
-
+      print("=" * 20)
+      print("Query text: {}".format(response.query_result.query_text))
+      print(
+          "Detected intent: {} (confidence: {})\n".format(
+              response.query_result.intent.display_name,
+              response.query_result.intent_detection_confidence,
+          )
+      )
+      print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
