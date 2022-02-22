0bd2127f5222fa7ebadd6a648b06515cc63ca740
Thuan Vo <thuan.votann@gmail.com>
2022-02-22 14:10:52 -0800

    Add __init__.py in app package

--- app/tests/test_store_info.py
+++ app/tests/test_store_info.py
@@ -1,6 +1,7 @@
 import pytest
-from products.product_info import StoreProductHandler
-from products.database import STORE_INFO
+from app.products.product_info import StoreProductHandler
+from app.products.database import STORE_INFO
+
 
 class TestStoreInfo:
     # def test_handler_name(self):
@@ -13,41 +14,49 @@ class TestStoreInfo:
         message = "Where is the store?"
         expectedOutput = "It is {}".format(STORE_INFO["address"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_city(self):
         classTest = StoreProductHandler()
         message = "Which city is the store at?"
         expectedOutput = "It is {}".format(STORE_INFO["city"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_province(self):
         classTest = StoreProductHandler()
         message = "Which province is the store at?"
         expectedOutput = "It is {}".format(STORE_INFO["province"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_postal(self):
         classTest = StoreProductHandler()
         message = "What is the postal code of the store?"
         expectedOutput = "It is {}".format(STORE_INFO["postal_code"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_country(self):
         classTest = StoreProductHandler()
         message = "Which country is the store at?"
         expectedOutput = "It is {}".format(STORE_INFO["country"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_phone(self):
         classTest = StoreProductHandler()
         message = "What is the store's phone number?"
         expectedOutput = "It is {}".format(STORE_INFO["phone"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_website(self):
         classTest = StoreProductHandler()
         message = "What is the store's website?"
         expectedOutput = "It is {}".format(STORE_INFO["website"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_opening_hours(self):
         classTest = StoreProductHandler()
         message = "What are the opening hours of the store?"
         expectedOutput = "It is {}".format(STORE_INFO["opening_hours"])
         assert(classTest.handle(message) == expectedOutput)
+
     def test_handler_price_range(self):
         classTest = StoreProductHandler()
         message = "What is the store's price range?"
