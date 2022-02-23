51c25288df237d7787171290ba84482371e94923
SanjanaCode <sanjanareddy.feb2001@gmail.com>
2022-02-23 15:31:55 -0800

    created furtherConcerns class

---
+++ app/concerns/further-concern.py
@@ -0,0 +1,2 @@
+class furtherConcerns:
+    pass
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from app.bot import Bot
 
 def main():
     bot = Bot()
-    bot.route_to_handler("product-info","what is apples?")
+    #bot.route_to_handler("product-info","what is apples?")
+    bot.detect_intent_texts("When does the store close?")
 
 
 if __name__ == '__main__':
