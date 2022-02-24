ab0f8e900f30d4ff3379f76926273958a579b3b5
Thuan Vo <thuan.votann@gmail.com>
2022-02-23 19:03:26 -0800

    Move utility conversion methods to separate class as static methods

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -1,5 +1,37 @@
 import pytest
 from app.products.database import MOCK_PRODUCT_DATA, SQLiteDatabase, DatabaseType
+from app.products.product_info import StoreProductHandler
+
+
+class ConvertUtilities:
+    # Utility method to convert result set into string
+    @staticmethod
+    def query_result_to_str(cursor):
+        result = []
+        for row in cursor:
+            row_data = []
+            for attr in row:
+                row_data.append(str(attr))
+
+            # Convert last attr
+            row_data[-1] = "1" if row_data[-1] else "0"
+            result.append(",".join(row_data))
+
+        return "\n".join(result)
+
+    # Utility method to convert result (pre-defined) set into string
+    @staticmethod
+    def all_data_to_str():
+        all_data = []
+        for row in MOCK_PRODUCT_DATA:
+            row_data = []
+            for _, v in row.items():
+                row_data.append(str(v))
+            # Convert last attr
+            row_data[-1] = "1" if row_data[-1] else "0"
+            all_data.append(",".join(row_data))
+
+        return "\n".join(all_data)
 
 
 @pytest.mark.database
@@ -27,8 +59,8 @@ class TestSQLiteDatabase:
         cursor = db.execute_query("SELECT * FROM product;")
 
         # Parse the result set to str
-        result_str = self.query_result_to_str(cursor)
-        expected_str = self.all_data_to_str()
+        result_str = ConvertUtilities.query_result_to_str(cursor)
+        expected_str = ConvertUtilities.all_data_to_str()
 
         # Checking
         assert result_str == expected_str
@@ -39,35 +71,8 @@ class TestSQLiteDatabase:
         cursor = db.execute_query("SELECT * FROM product;")
 
         # Parse the result set to str
-        result_str = self.query_result_to_str(cursor)
-        expected_str = self.all_data_to_str()
+        result_str = ConvertUtilities.query_result_to_str(cursor)
+        expected_str = ConvertUtilities.all_data_to_str()
 
         # Checking
         assert result_str == expected_str
-
-    # Utility method to convert result set into string
-    def query_result_to_str(self, cursor):
-        result = []
-        for row in cursor:
-            row_data = []
-            for attr in row:
-                row_data.append(str(attr))
-
-            # Convert last attr
-            row_data[-1] = "1" if row_data[-1] else "0"
-            result.append(",".join(row_data))
-
-        return "\n".join(result)
-
-    # Utility method to convert result (pre-defined) set into string
-    def all_data_to_str(self):
-        all_data = []
-        for row in MOCK_PRODUCT_DATA:
-            row_data = []
-            for _, v in row.items():
-                row_data.append(str(v))
-            # Convert last attr
-            row_data[-1] = "1" if row_data[-1] else "0"
-            all_data.append(",".join(row_data))
-
-        return "\n".join(all_data)
