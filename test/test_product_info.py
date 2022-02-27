291e52fde92f78a73057e511829ca4d78275b550
paulgray01 <paulgray01@icloud.com>
2022-02-27 11:43:58 -0800

    edited product info tests

--- test/test_product_info.py
+++ test/test_product_info.py
@@ -15,9 +15,9 @@ class TestProductInfo:
         expectedOutput = "%s cost $%s %s." % (MOCK_PRODUCT_DATA[0]["names"].capitalize(), MOCK_PRODUCT_DATA[0]["price"], MOCK_PRODUCT_DATA[0]["price_scale"])
         assert(classTest.handle(message) == expectedOutput)
 
-    def test_handler_stockbanana(self, classTest):
-        message = "How many bananas do you have?"
-        expectedOutput = "{} are in stock.".format(MOCK_PRODUCT_DATA[0]["names"].capitalize())
+    def test_handler_stockapple(self, classTest):
+        message = "How many apples do you have?"
+        expectedOutput = "{} are in stock.".format(MOCK_PRODUCT_DATA[2]["names"].capitalize())
         assert(classTest.handle(message) == expectedOutput)
 
     def test_handler_priceberry(self, classTest):
@@ -25,9 +25,9 @@ class TestProductInfo:
         expectedOutput = "%s cost $%s %s." % (MOCK_PRODUCT_DATA[1]["names"].capitalize(), MOCK_PRODUCT_DATA[1]["price"], MOCK_PRODUCT_DATA[1]["price_scale"])
         assert(classTest.handle(message) == expectedOutput)
 
-    def test_handler_stockberry(self, classTest):
-        message = "Are strawberries in stock?"
-        expectedOutput = "{} are in stock.".format(MOCK_PRODUCT_DATA[1]["names"].capitalize())
+    def test_handler_stockpear(self, classTest):
+        message = "Are pears in stock?"
+        expectedOutput = "{} are out of stock.".format(MOCK_PRODUCT_DATA[3]["names"].capitalize())
         assert(classTest.handle(message) == expectedOutput)
 
     
