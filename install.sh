18b2c5c0caaa7bd04a3dccebc0c48fb0d6f05f83
Thuan Vo <thuan.vo@ubc.ca>
2022-02-27 14:11:03 -0800

    Revert install script to support only Unix

--- install.sh
+++ install.sh
@@ -3,22 +3,8 @@
 # Create a virtual environment and install the requirements
 python3 -m venv venv
 
+# Activate the environment
+source ./venv/bin/activate
 
-# Check the platform
-OS="$(uname -s)"
-
-# If we're on Windows
-if [[  $OS =~ MINGW.* || $OS =~ CYGWIN.* ]]; then
-    # Activate the environment
-    exec ./venv/Scripts/activate
-    # Install the packages
-    exec ./venv/bin/pip3 install -r requirements.txt
-else
-    # Activate the environment
-    source ./venv/bin/activate
-
-    # Install the packages
-    ./venv/bin/pip3 install -r requirements.txt
-fi
-
-
+# Install the packages
+./venv/bin/pip3 install -r requirements.txt
