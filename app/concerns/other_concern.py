1ac0ac924a90a7d1f786ca08da5c820a68381d11
SanjanaCode <sanjanareddy.feb2001@gmail.com>
2022-03-17 11:53:18 -0700

    formatted print statements and added class definition

--- app/concerns/other_concern.py
+++ app/concerns/other_concern.py
@@ -1,4 +1,10 @@
 class OtherConcerns:
+    """
+    This class contains methods that handle all questions that relate to other concerns about returns and refunds.
+    It also handles cases when the bot is unable to understand what the user types.
+    Parameters: None
+    Attributes: None
+    """
     
     def handle(self, sentimentNum, intent): 
         """
@@ -6,15 +12,16 @@ class OtherConcerns:
         parameters: 
                     sentimentNum - denotes whether the text from customer is a positive or negative sentiment.
                     intent - holds a string representing the intent detected.
-        returns: nothing
+        returns: Nothing
         """
         # If sentimentNum is a negative number then the text denotes a negative sentiment. 
         # If sentimentNum is a positive number then the text denotes a positive sentiment.
         # If sentimentNum is equal to zero, it's a neutral sentiment so print nothing.
+        #print("test: ",sentimentNum)
         if(sentimentNum<0):     
-            print("We're sorry to hear that!")
+            print("Bot: Sorry to hear that!")
         if(sentimentNum>0):
-            print("That's great! Thank you for your feedback!")
+            print("Bot: That's great! Thank you for your feedback!")
         
         #If customer is asking for an exchange, direct it to the handleExchange() method.
         if(intent=="exchange-request"):
@@ -50,16 +57,16 @@ class OtherConcerns:
     def handleExchange(self):
         """
         Handles requests for exchanges
-        parameters: nothing
-        returns: nothing
+        parameters: None
+        returns: Nothing
         """
-        print("Bot: You can exchange the product within 2 weeks (if perishable, then within 1-2 days) of purchase by visiting our store.\n Please ensure that: \n1. the product is unused \n2. the price tags are intact \n3. you bring the bill along with the product.")
+        print("Bot: You can exchange the product within 2 weeks (if perishable, then within 1-2 days) of purchase by visiting our store.\nPlease ensure that: \n1. the product is unused \n2. the price tags are intact \n3. you bring the bill along with the product.")
 
 
     def handleRefunds(self):
         """
         Handles requests for refunds
-        parameters: nothing
-        returns: nothing
+        parameters: None
+        returns: Nothing
         """
-        print("Bot: You can request for a refund or return in 2 ways:  1. place a request on our website(our agent will come to pick up the product) \n 2. directly visit our store\n Note that all requests for refunds or returns have to be made within 2 weeks of purchasing.\n After your refund/return is processed, the money will be refunded \n 1. To your original payment method (if paid by credit/debit card\n 2. As store credit")
+        print("Bot: You can request for a refund or return in 2 ways:\n1. place a request on our website(our agent will come to pick up the product)\n2. directly visit our store\nNote that all requests for refunds or returns have to be made within 2 weeks of purchasing.\nAfter your refund/return is processed, the money will be refunded either:\n1. To your original payment method (if paid by credit/debit card\n2. Or as store credit")
